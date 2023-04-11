e72a0b10a4faba849901cf0bc5d3bdb218c2a76e
fondlykip <liam.j.campbell@hotmail.co.uk>
2023-04-11 04:43:57 +0200

    update query

--- src/pipelines/sql/INSERT_STG_PRD.sql
+++ src/pipelines/sql/INSERT_STG_PRD.sql
@@ -2,4 +2,4 @@ INSERT INTO {{ params.psql_prd_table }}
     SELECT * FROM {{ params.psql_staging_table }}
     ON CONFLICT (trip_id) DO NOTHING;
 
-/* DROP TABLE {{ params.psql_staging_table }}; */
+DROP TABLE {{ params.psql_staging_table }};
