64dc5ec5321927e196c80b44028b83f85bb9a29b
fondlykip <liam.j.campbell@hotmail.co.uk>
2023-04-12 21:45:22 +0200

    update sql query and add basic api logic

--- src/pipelines/sql/INSERT_STG_PRD.sql
+++ src/pipelines/sql/INSERT_STG_PRD.sql
@@ -1,5 +1,51 @@
-INSERT INTO {{ params.psql_prd_table }} 
-    SELECT * FROM {{ params.psql_staging_table }}
+INSERT INTO {{ params.psql_prd_table }} (
+    trip_id,
+    taxi_id,
+    trip_start_timestamp,
+    trip_end_timestamp,
+    trip_seconds,
+    trip_miles,
+    pickup_census_tract,
+    dropoff_census_tract,
+    pickup_community_area,
+    dropoff_community_area,
+    fare,
+    tips,
+    tolls,
+    extras,
+    trip_total,
+    payment_type,
+    company,
+    pickup_centroid_latitude,
+    pickup_centroid_longitude,
+    pickup_centroid_location,
+    dropoff_centroid_latitude,
+    dropoff_centroid_longitude,
+    dropoff_centroid_location)
+    SELECT  trip_id,
+            taxi_id,
+            trip_start_timestamp,
+            trip_end_timestamp,
+            trip_seconds,
+            trip_miles,
+            pickup_census_tract,
+            dropoff_census_tract,
+            pickup_community_area,
+            dropoff_community_area,
+            fare,
+            tips,
+            tolls,
+            extras,
+            trip_total,
+            payment_type,
+            company,
+            pickup_centroid_latitude,
+            pickup_centroid_longitude,
+            pickup_centroid_location,
+            dropoff_centroid_latitude,
+            dropoff_centroid_longitude,
+            dropoff_centroid_location
+    FROM {{ params.psql_staging_table }}
     ON CONFLICT (trip_id) DO NOTHING;
 
 DROP TABLE {{ params.psql_staging_table }};
