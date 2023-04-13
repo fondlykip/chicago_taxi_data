bb3c236022876349c409aa6563ee477afabb19ab
fondlykip <liam.j.campbell@hotmail.co.uk>
2023-04-13 13:26:49 +0200

    update params call

--- src/pipelines/sql/create_prd_tbl.sql
+++ src/pipelines/sql/create_prd_tbl.sql
@@ -1,24 +1,24 @@
-CREATE TABLE IF NOT EXISTS {{ params.comm_area_table }}(
+CREATE TABLE IF NOT EXISTS {{ params.psql_comm_area_table }}(
     community_area_id SMALLINT PRIMARY KEY,
     community_area_name TEXT,
     community_area_size BIGINT
 );
 
-CREATE TABLE IF NOT EXISTS {{ params.trip_table }}(
+CREATE TABLE IF NOT EXISTS {{ params.psql_trip_fact_table }}(
     trip_id TEXT PRIMARY KEY,
     taxi_id TEXT,
     trip_start_timestamp TIMESTAMP,
-    trip_start_date_id TEXT references {{ params.date_dim }},
+    trip_start_date_id TEXT references {{ params.psql_date_dim_table }},
     trip_start_time TIME,
     trip_end_timestamp TIMESTAMP,
-    trip_end_date_id TEXT references {{ params.date_dim }},
+    trip_end_date_id TEXT references {{ params.psql_date_dim_table }},
     trip_end_time TIME,
     trip_seconds INT,
     trip_miles FLOAT(2),
     pickup_census_tract TEXT,
     dropoff_census_tract TEXT,
-    pickup_community_area SMALLINT references {{ params.comm_area_table }}(community_area_id),
-    dropoff_community_area SMALLINT references {{ params.comm_area_table }}(community_area_id),
+    pickup_community_area SMALLINT references {{ params.psql_comm_area_table }}(community_area_id),
+    dropoff_community_area SMALLINT references {{ params.psql_comm_area_table }}(community_area_id),
     fare MONEY,
     tips MONEY,
     tolls MONEY,
